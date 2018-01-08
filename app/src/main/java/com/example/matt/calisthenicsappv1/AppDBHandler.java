20b1097020a26b22e963f751623e269b8553e8ea
Sepriam <34950923+Sepriam@users.noreply.github.com>
2018-01-08 06:05:36 +0000

    Updated db handler java class with comments on functionality

--- app/src/main/java/com/example/matt/calisthenicsappv1/AppDBHandler.java
+++ app/src/main/java/com/example/matt/calisthenicsappv1/AppDBHandler.java
@@ -5,4 +5,28 @@ package com.example.matt.calisthenicsappv1;
  */
 
 public class AppDBHandler {
+
+    /*
+    Requirements:
+
+    1) Functions for database:
+    1.1 - Create SQLite database
+    1.1.1 - execSQL(Table creation Functionality)
+    1.2 - Update SQLite database
+    1.3 - Delete SQLite database
+
+    2) Functions for Table:
+    2.1 - Create Table & Column Headers
+    2.1.1 - Create column header for exerciseObject variables
+    2.2 - Create Functions to add & remove objects to database
+    2.3 - Create Function to update the object in table ((IF check then update isChecked Value))
+
+
+    NOTE -- This may be acceptable to create in a separate java class
+    3) First Time Load Table & Content Creation Functions:
+    3.1 - Create a list of all exercises from exercise index excel spreadsheet
+    3.2 - Import List into tables
+
+     */
+
 }
